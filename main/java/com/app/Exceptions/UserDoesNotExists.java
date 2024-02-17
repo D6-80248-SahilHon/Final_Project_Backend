dfd7ade71b3c23f8ae40512b0000eac31112bd34
D6-80248-SahilHon <137618847+D6-80248-SahilHon@users.noreply.github.com>
2024-02-17 22:19:35 +0530

    Delete main directory

--- main/java/com/app/Exceptions/UserDoesNotExists.java
+++
@@ -1,7 +0,0 @@
-package com.app.Exceptions;
-
-public class UserDoesNotExists extends RuntimeException{
-    public UserDoesNotExists() {
-        super("User does not exists");
-    }
-}
--- main/java/com/app/types/SeatType.java
+++
@@ -1,6 +0,0 @@
-package com.app.types;
-
-public enum SeatType {
-    CLASSIC,
-    PREMIUM
-}
